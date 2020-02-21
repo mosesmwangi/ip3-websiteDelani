9186a71ead7c02ca2896eb31ce139f460f869532
mosesmwangi <mosesmwangi286@gmail.com>
2020-02-21 16:44:40 +0300

    add the transparent color to the inputs and textarea

--- js/sripts.js
+++ js/sripts.js
@@ -15,6 +15,6 @@ $(document).ready(function() {
         $(this).text("image");
         },
         function() {
-            $(this).show();
+            $(this).show("image show");
         });
     });
