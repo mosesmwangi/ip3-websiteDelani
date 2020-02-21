5867541e9977500d570e72c2112023064e21ae13
mosesmwangi <mosesmwangi286@gmail.com>
2020-02-21 07:52:05 +0300

    add the toggle show and hide for the design, development and product development parts

---
+++ js/sripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#design").click(function() {
+        $("#hide1").toggle();
+    })
+    $("#devt").click(function() {
+        $("#hide2").toggle();
+    })
+    $("#prodDevt").click(function() {
+        $("#hide3").toggle();
+    })
+})
