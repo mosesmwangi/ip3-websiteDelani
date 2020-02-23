c75b392d851b5f47fc31e7d6935ac1b200cde0f7
mosesmwangi <mosesmwangi286@gmail.com>
2020-02-23 06:31:48 +0300

    add the correct jquery functionality to the design, development and product management icons

--- js/sripts.js
+++ js/sripts.js
@@ -4,22 +4,37 @@ function returnMessage() {
 
 
 $(document).ready(function() {
-    $("#design").click(function() {
+    $("#designShow").click(function() {
         $("#hide1").toggle();
-    })
-    $("#devt").click(function() {
+        $("#designShow").toggle();
+    });
+    $("#hide1").click(function() {
+        $("#designShow").toggle();
+        $("#hide1").toggle();
+    });
+    $("#devShow").click(function() {
         $("#hide2").toggle();
-    })
-    $("#prodDevt").click(function() {
+        $("#devShow").toggle();
+    });
+    $("#hide2").click(function() {
+        $("#devShow").toggle();
+        $("#hide2").toggle();
+    });
+    $("#productShow").click(function() {
         $("#hide3").toggle();
-    })
-    $(".col-md-3").hover(function() 
+        $("#productShow").toggle();
+    });
+    $("#hide3").click(function() {
+        $("#productShow").toggle();
+        $("#hide3").toggle();
+    });
+    $(".col-md-3").mouseover(function() 
     {
-        $(this).text("image");
-        },
-        function() {
-            $(this).show("image show");
+        $(this).css("opacity","0.5");
         });
+        $(".col-md-3").mouseout(function() {
+            $(this).css("opacity","1");
+        })
     $("#form").submit(function(event) {
         var inKeyed = $(".input").val();
         if(inKeyed != null) {
